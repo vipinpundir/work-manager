969ad62ea703210e9ce5603612388f28be9b2d38
Vipin Pundir <vipinpundirmzn001@gmail.com>
2024-07-30 15:56:44 +0530

    add loading

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import Image from 'next/image';
+import logo from '@/assets/logo.png'
+const Spinner = () => {
+  return (
+    <div className="absolute h-[100vh] w-[100vw] flex justify-center items-center backdrop-blur-md bg-white/30">
+      <div className="w-12 h-12 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+      <div className="absolute w-10 h-10">
+        <Image src={logo} alt="Logo" layout="fill" objectFit="contain" />
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -1,21 +1,24 @@
 "use client"
+import Spinner from '@/components/Spinner';
 import userService from '@/services/userService';
 import { createContext, useState, ReactNode, useEffect } from 'react';
 
 export const UserContext = createContext({});
 
-// Create a provider component
+// context provider 
 export const UserProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState(undefined);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
     const fetchCurrentUser = async () => {
       try {
         const response = await userService.currentUser();
         setUser(response?.data)
       } catch (error) {
         setUser(undefined)
+      }finally{
+        setLoading(false)
       }
     }
     fetchCurrentUser()
@@ -25,6 +28,7 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
+      {loading? <Spinner /> : ''}
       {children}
     </UserContext.Provider>
   );
